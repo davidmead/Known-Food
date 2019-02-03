6c4157b894c2c22aa8396949168594f6ad2b6eff
David Mead <davidjohnmead@gmail.com>
2019-02-03 13:59:21 -0500

    update title to include food & drink

--- ContentType.php
+++ ContentType.php
@@ -4,8 +4,8 @@
 
         class ContentType extends \Idno\Common\ContentType {
 
-            public $title = 'Food';
-            public $category_title = 'Food';
+            public $title = 'Eat/Drink';
+            public $category_title = 'Eat/Drink';
             public $entity_class = 'IdnoPlugins\\Food\\Food';
             public $logo = '<i class="icon-align-left"></i>';
             public $indieWebContentType = array('food');
